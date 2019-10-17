2dfdd7614d56a7724cef3e918507a65d9a543281
juan siu <juan.siu@gmail.com>
2019-10-17 01:05:55 +0000

    sessions index edit edit completed working on new and show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,85 @@
+require './config/environment'
+#require 'pry'
+
 class SessionsController < Sinatra::Base
+  #require configuration for sessions and password to work
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+  #bring the index website of the object
+    get "/sessions" do
+        @photographers = Photographer.all
+        @sessions = Session.all
+        @clients = Client.all
+        erb :"/sessions/index"
+    end
+#open up the form for the new client
+    get "/sessions/new" do
+      if logged_in?
+        erb :'/sessions/new'
+      else
+        redirect "/login"
+      end
+    end
+  #take form params from new to create new client under the current user
+    post "/sessions" do
+      if logged_in?
+        if !params[:type].empty? && !params[:price].empty? && !params[:date].empty? && !params[:duration].empty? && !params[:rating].empty? && !params[:status].empty? && !params[:location].empty?
+          @session = Session.new(:type => params[:type], :price => params[:price], :date => params[:date], :duration => params[:duration], :rating => params[:rating], :status => params[:status], :location => params[:location], :photographer_id => current_user.id)
+          @session.save
+          redirect "/sessions"
+        else
+          redirect "/sessions"
+        end
+      else
+        redirect "/login"
+      end
+    end
 
+  #open up the details page of the object
+    get "/sessions/:id" do
+      @session = Session.find_by_id(params[:id])
+      @clients = Client.all
+      @photographers = Photographer.all
+      erb :"/sessions/show"
+    end
+  #open up the editing form for the object
+    get "/sessions/:id/edit" do
+      @session = Session.find_by_id(params[:id])
+      @clients = Client.all
+      @photographers = Photographer.all
+      erb :'/clients/edit'
+    end
+  #take params from edit to update the data of the object
+    patch "/sessions/:id" do
+      @session = Session.find_by_id(params[:id])
+      @session.update(:type => params[:type], :price => params[:price], :date => params[:date], :duration => params[:duration], :rating => params[:rating], :status => params[:status], :location => params[:location], :photographer_id => current_user.id)
+      @clients = Client.all
+      @session[:client_id] = @artists.last.id
+      redirect "/sessions"
+    end
+  #open up the delete website for client
+    get "/sessions/:id/delete" do
+      @session = Session.find_by_id(params[:id])
+      erb :'clients/delete'
+    end
+  #accept the request from delete, and delete object
+    delete '/sessions/:id' do
+        @session = Session.find_by_id(params[:id])
+        @session.delete
+        redirect to '/session'
+    end
+#Additional Methods for login authentication
+    helpers do
+      def logged_in?
+        !!session[:photographer_id]
+      end
 
-
-
+      def current_user
+        Photographer.find(session[:photographer_id])
+      end
+    end
 end
