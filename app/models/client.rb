9e9bc01fd6ad645b5b548577a26e7952fe4b84eb
juan siu <juan.siu@gmail.com>
2019-10-26 16:55:30 +0000

    added uniqueness on clients emails as well

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,8 @@
 class Client < ActiveRecord::Base
   #extend Slugifiable::ClassMethods
   #include Slugifiable::InstanceMethods
+  validates :email,    :presence => true,
+                       :uniqueness => true
 
   has_many :sessions
   has_many :photographers, through: :sessions
